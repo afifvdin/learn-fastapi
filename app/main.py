17b87146bb8b704e61167be7d0c4d718cffe429f
afifvdin <afifudin.fdn@gmail.com>
2025-09-08 23:03:58 +0700

    fix

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import Depends, FastAPI
+from fastapi import Depends, FastAPI, HTTPException
 from fastapi.responses import HTMLResponse
 from scalar_fastapi import get_scalar_api_reference
 from sqlmodel import Session, select
@@ -17,6 +17,14 @@ def get_notes(db: Session = Depends(get_db_session)):
     return db.exec(select(Note)).all()
 
 
+@app.get("/notes/{id}", response_model=NoteRead)
+def get_note(id: int, db: Session = Depends(get_db_session)):
+    note = db.get(Note, id)
+    if not note:
+        raise HTTPException(404, "Note not found")
+    return note
+
+
 @app.post("/notes", response_model=Note)
 def create_note(dto: NoteCreate, db: Session = Depends(get_db_session)):
     note = Note(title=dto.title, content=dto.content)
@@ -25,6 +33,29 @@ def create_note(dto: NoteCreate, db: Session = Depends(get_db_session)):
     db.refresh(note)
 
     return note.model_dump()
+
+
+@app.patch("/notes/{id}", response_model=NoteRead)
+def update_note(id: int, dto: NoteCreate, db: Session = Depends(get_db_session)):
+    note = db.get(Note, id)
+    if not note:
+        raise HTTPException(404, "Note not found")
+    note.title = dto.title
+    note.content = dto.content
+    db.commit()
+    db.refresh(note)
+    return note
+
+
+@app.delete("/notes/{id}", response_model=NoteRead)
+def delete_note(id: int, db: Session = Depends(get_db_session)):
+    note = db.get(Note, id)
+    if not note:
+        raise HTTPException(404, "Note not found")
+    db.delete(note)
+    db.commit()
+
+    return note
 
 
 @app.get("/scalar", include_in_schema=False)
